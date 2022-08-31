b5e5b1f11efbf425437d3cbf44ef35336d485ca0
tKons64 <spb_unex@mail.ru>
2022-08-31 13:09:28 +0300

    Выполненно задание №2

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,27 @@ public class Main {
         System.out.println("Значение переменной varTypeFloat, с типом float, равно - " + varTypeFloat);
         System.out.println("Значение переменной varTypeDouble, с типом double, равно - " + varTypeDouble);
 
+    //Задание №2
+        System.out.println(" ");
+        System.out.println("Задание №2:");
+        float var1 = 27.12f;
+        long var2 = 987_678_965_549L;
+        float var3 = 2.786f;
+        boolean var4 = false;
+        short var5 = 569;
+        short var6 = -159;
+        short var7 = 27897;
+        byte var8 = 67;
+
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(var4);
+        System.out.println(var5);
+        System.out.println(var6);
+        System.out.println(var7);
+        System.out.println(var8);
+
 
     }
 }
