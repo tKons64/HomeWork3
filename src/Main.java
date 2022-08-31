d347c5e0afbc3a20d0c2bb830d1e5606283460cb
tKons64 <spb_unex@mail.ru>
2022-08-31 13:07:04 +0300

    Выполненно задание №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    //Задание №1
+        System.out.println(" ");
+        System.out.println("Задание №1:");
+        byte varTypeByte = 1;
+        short varTypeShort = 200;
+        int varTypeInt = 3000;
+        long varTypeLong = 4 ;
+
+        float varTypeFloat = 1.5f;
+        double varTypeDouble = 2.5;
+
+        System.out.println("Значение переменной varTypeByte, с типом byte, равно - " + varTypeByte);
+        System.out.println("Значение переменной varTypeShort, с типом short, равно - " + varTypeShort);
+        System.out.println("Значение переменной varTypeInt, с типом int, равно - " + varTypeInt);
+        System.out.println("Значение переменной varTypeLong, с типом long, равно - " + varTypeLong);
+        System.out.println("Значение переменной varTypeFloat, с типом float, равно - " + varTypeFloat);
+        System.out.println("Значение переменной varTypeDouble, с типом double, равно - " + varTypeDouble);
+
+
     }
 }
